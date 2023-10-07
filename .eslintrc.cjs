284ea56d211593ad0779280752012f234d0b91c2
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-10-07 22:11:02 +0900

    chore: ESLint, Prettier, Husky 환경 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json', 
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
